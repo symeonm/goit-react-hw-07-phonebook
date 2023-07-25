4456f2c88c4da92403c97ca51fbddd88d33dd7e8
Symeon Melnychuk <m.simeon998@gmail.com>
2023-07-25 14:31:11 +0300

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operation';
 import ContactForm from '../contact-form/ContactForm';
 import ContactList from '../contact-list/ContactList';
 import Filter from '../filter/Filter';
 
 export function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(fetchContacts())
-  }, [dispatch])
+  const isLoading = useSelector(state => state.contacts.isLoading);
+  const error = useSelector(state => state.contacts.error);
+
+  // console.log(isLoading);
+  // console.log(error);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div>
+      {isLoading && !error && <b>Request in progress...</b>}
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
